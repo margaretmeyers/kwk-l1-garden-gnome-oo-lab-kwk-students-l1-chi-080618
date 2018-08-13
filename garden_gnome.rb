f59f37a1f95f13d227f3817caad0171856f65182
margaretmeyers <marg.m.meyers@gmail.com>
2018-08-13 14:05:23 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,31 @@
 # Code your instances here
+class GardenGnome
+  def initialize(personality="evil", hat_color="red")
+    @personality="evil" 
+    @hat_color="red"
+  end 
+  def personality
+    @personality = "evil"
+  end
+  
+  
+  def name=(name)
+    @name = name
+  end 
+  def name 
+    @name 
+  end 
+  def age=(age)
+    @age = age 
+  end 
+  def age
+    @age 
+  end 
+  def gluten_allergy=(gluten_allergy)
+    @gluten_allergy = gluten_allergy
+  end 
+  def gluten_allergy
+    @gluten_allergy
+  end
+  
+end 
